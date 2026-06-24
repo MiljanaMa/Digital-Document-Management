755ec1a5167439c4fbeca54623f4b56c695c195f
MiljanaMa <miljana.marjanovic9@gmail.com>
2026-06-24 21:34:36 +0200

    feat: fix kibana url

--- ddm-frontend/src/components/Dashboard.js
+++ ddm-frontend/src/components/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Dashboard() {
   return (
     // eslint-disable-next-line jsx-a11y/iframe-has-title
-    <iframe src="http://localhost:5601/app/dashboards#/view/ebe37c48-d806-4bd3-9874-984c7b76540f?_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3A'2025-10-01T19%3A00%3A00.000Z'%2Cto%3Anow))&show-top-menu=true&show-query-input=true&show-time-filter=true" 
+    <iframe src="http://localhost:18080/kibana/app/dashboards#/view/ebe37c48-d806-4bd3-9874-984c7b76540f?_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3A'2025-10-01T19%3A00%3A00.000Z'%2Cto%3Anow))&show-top-menu=true&show-query-input=true&show-time-filter=true"
     height="800" width="100%"></iframe>
   )
 }
